d85700658a5c712827a41b6eac8dfc2cf16cdc19
Javier <sjavierlopez@gmail.com>
2020-09-29 00:14:32 -0500

    Let's go

--- webServer.go
+++ webServer.go
@@ -18,6 +18,7 @@ func myHandler(w http.ResponseWriter, r *http.Request) {
 func timeHandler(w http.ResponseWriter, r *http.Request) {
 	t := time.Now().Format(time.RFC1123)
 	Body := "The current time is:"
+	//Simple test
 	fmt.Fprintf(w, "<h1 align=\"center\">%s</h1>", Body)
 	fmt.Fprintf(w, "<h2 align=\"center\">%s</h2>\n", t)
 	fmt.Fprintf(w, "Serving: %s\n", r.URL.Path)
